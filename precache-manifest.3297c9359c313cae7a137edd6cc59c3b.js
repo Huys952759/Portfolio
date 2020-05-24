27daf87579e84dda1a43ca1ed4fe74f7026a7f13
Yaosong Hu <yaosonghu@Yaosongs-MacBook-Pro.local>
2020-05-24 16:31:11 +0800

    Updates

---
+++ precache-manifest.3297c9359c313cae7a137edd6cc59c3b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46b27fcef422597c39fe2091a0f29fc8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "741c85a199344039409b",
+    "url": "/Portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "42034f0a9f2f16607cc7",
+    "url": "/Portfolio/static/css/main.d00a118f.chunk.css"
+  },
+  {
+    "revision": "741c85a199344039409b",
+    "url": "/Portfolio/static/js/2.f4be485a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.f4be485a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42034f0a9f2f16607cc7",
+    "url": "/Portfolio/static/js/main.90698c9f.chunk.js"
+  },
+  {
+    "revision": "107ce3c625a9fb1de279",
+    "url": "/Portfolio/static/js/runtime-main.e029fae7.js"
+  },
+  {
+    "revision": "cc466e57cfafae37edb7de4bb1291760",
+    "url": "/Portfolio/static/media/Azure.cc466e57.png"
+  },
+  {
+    "revision": "b61184ef50dc403411cbe2a1034baaff",
+    "url": "/Portfolio/static/media/CSU.b61184ef.png"
+  },
+  {
+    "revision": "6d57b8f4d3effc61d37cd8f9119dcce0",
+    "url": "/Portfolio/static/media/DrumMachine.6d57b8f4.png"
+  },
+  {
+    "revision": "d3f0521a64c1aba0b5ad851c2b6154ba",
+    "url": "/Portfolio/static/media/Government.d3f0521a.png"
+  },
+  {
+    "revision": "cd95acac205130d5114bd2c4eb22eb13",
+    "url": "/Portfolio/static/media/Hidilao.cd95acac.png"
+  },
+  {
+    "revision": "ec6c4f005f0c5daefb36dd7afd161e10",
+    "url": "/Portfolio/static/media/JWR.ec6c4f00.png"
+  },
+  {
+    "revision": "a85267007151b9a2fe186d41cb2a9caf",
+    "url": "/Portfolio/static/media/JavaScriptCalculator.a8526700.png"
+  },
+  {
+    "revision": "c6849eb1d6bd3cf23fe7b2d6d3eed291",
+    "url": "/Portfolio/static/media/MarkdownPreviewer.c6849eb1.png"
+  },
+  {
+    "revision": "b55b6b6c623808a9e2a05c7ff101fee9",
+    "url": "/Portfolio/static/media/Photograph.b55b6b6c.png"
+  },
+  {
+    "revision": "791992ceafa774ed5edd006ce02904f7",
+    "url": "/Portfolio/static/media/PomodoroClock.791992ce.png"
+  },
+  {
+    "revision": "ffe492907fc5953ee43135a92dfb87c7",
+    "url": "/Portfolio/static/media/Time.ffe49290.png"
+  },
+  {
+    "revision": "310571884d6a27b76d2461b53257b81c",
+    "url": "/Portfolio/static/media/UBC.31057188.png"
+  },
+  {
+    "revision": "d4537fee4c0cb6b2c82ed2f8461a85e1",
+    "url": "/Portfolio/static/media/Weather.d4537fee.png"
+  },
+  {
+    "revision": "b34704877a538652da211e2925f25285",
+    "url": "/Portfolio/static/media/random.b3470487.png"
+  }
+]);
